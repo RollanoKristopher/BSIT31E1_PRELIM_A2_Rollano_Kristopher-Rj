f665d489016df546f2fb6794100627dbba6f07f1
Kristopher Rj Rollano <kristopherrjrollano@gmail.com>
2026-06-15 10:08:37 +0800

    Added calculator operations

--- BSIT31E1_PRELIM_A2_Rollano_Kristopher-Rj/Program.cs
+++ BSIT31E1_PRELIM_A2_Rollano_Kristopher-Rj/Program.cs
@@ -11,11 +11,55 @@
         break;
 
     }
+    if (!double.TryParse(input, out double num1))
+        {
+        Console.WriteLine("Invalid number.");
+        continue;
+    }
 
-    Console.WriteLine("Enter Second Numner: ");
+    Console.Write("Enter Second Numner: ");
     string secondInput = Console.ReadLine();
+    
+    if (!double.TryParse(secondInput, out double num2))
 
-    Console.WriteLine("Enter Operator ( +, -, *, /): ");
+{
+    Console.WriteLine("Invalid number.");
+    continue;
+}
+
+Console.WriteLine("Enter Operator ( +, -, *, /): ");
     string Operator = Console.ReadLine();
-   
+
+    double result = 0;
+
+
+    if (Operator == "+")
+    {
+        result = num1 + num2;
+    }
+    else if (Operator == "-")
+    {
+        result = num1 - num2;
+    }
+    else if (Operator == "*")
+    {
+        result = num1 * num2;
+    }
+    else if (Operator == "/")
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Invalid.");
+            continue;
+        }
+
+        result = num1 / num2; 
+
+        }
+    else
+    {
+        Console.WriteLine("Invalid Operator.");
+        continue;
+    }
+    Console.WriteLine($"Result: {result}");
 }
