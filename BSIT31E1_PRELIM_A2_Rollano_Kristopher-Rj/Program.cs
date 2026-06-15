39d935ae1508afc8715519468f5f6516b41a68c8
Kristopher Rj Rollano <kristopherrjrollano@gmail.com>
2026-06-15 09:50:13 +0800

    Added calculator loop structure

--- BSIT31E1_PRELIM_A2_Rollano_Kristopher-Rj/Program.cs
+++ BSIT31E1_PRELIM_A2_Rollano_Kristopher-Rj/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.WriteLine("Enter Fist Number (or 'Exit' to quit): ");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "exit")
+        
+    {
+        //CODE
+        Console.WriteLine("Calculator closed.");
+        break;
+
+    }
+
+    Console.WriteLine("Enter Second Numner: ");
+    string secondInput = Console.ReadLine();
+
+    Console.WriteLine("Enter Operator ( +, -, *, /): ");
+    string Operator = Console.ReadLine();
+   
+}
